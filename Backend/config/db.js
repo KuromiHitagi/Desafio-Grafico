a0d1f97db9b46a85d5a6733aa8b986d84013ecec
Renan <nrenan2007@gmail.com>
2025-10-15 13:22:41 -0300

    revert db.js

--- Backend/config/db.js
+++ Backend/config/db.js
@@ -4,11 +4,11 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 const connection = await mysql.createConnection({
-  host: process.env.DB_HOST',
-  user: process.env.DB_USER',
-  password: process.env.DB_PASS',
-  database: process.env.DB_NAME',
-  port: process.env.DB_PORT,
+  host: process.env.DB_HOST || 'localhost',
+  user: process.env.DB_USER || 'root',
+  password: process.env.DB_PASS || '1234',
+  database: process.env.DB_NAME || 'grafico',
+  port: process.env.DB_PORT || 3306,
   ssl: { rejectUnauthorized: false }
 });
 
